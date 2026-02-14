33891c23842d81de2ec76fae5a8a87cd982827f4
jruiz002 <josegrg04@gmail.com>
2026-02-14 17:20:11 +0000

    ejercicio 2 terminado

---
+++ exercise_2/medicion_concurrente.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main() {
+
+    pid_t pid1, pid2, pid3;
+    clock_t inicio, fin;
+
+    inicio = clock();
+    pid1 = fork();
+
+    if (pid1 == 0) {
+
+        pid2 = fork();
+
+        if (pid2 == 0) {
+
+            pid3 = fork();
+
+            if (pid3 == 0) {
+                // BISNIETO
+                for (int i = 0; i < 1000000; i++) {
+                }
+            } else {
+                // NIETO
+                for (int i = 0; i < 1000000; i++) {
+                }
+                wait(NULL);
+            }
+
+        } else {
+            // HIJO
+            for (int i = 0; i < 1000000; i++) {
+            }
+            wait(NULL);
+        }
+
+    } else {
+        // PADRE
+        wait(NULL);
+        fin = clock();
+
+        double tiempo = (double)(fin - inicio);
+        printf("%f\n", tiempo);
+    }
+
+    return 0;
+}
+
